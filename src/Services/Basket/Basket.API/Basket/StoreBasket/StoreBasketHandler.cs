87dbe2385189b1317ad738779a10069be27a2763
Abubakar Siddiq <Abubakar Siddiq@DESKTOP-99FVISD>
2024-03-24 01:00:05 +0500

    basket api implementation

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketValidator()
+    {
+        RuleFor(command => command.Cart).NotNull().WithMessage("Cart can not be null.");
+        RuleFor(command => command.Cart.UserName).NotNull().WithMessage("UserName is required.");
+    }
+}
+
+public class StoreBasketCommandHandler(IBasketRepository repository)
+    : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        var cart = command.Cart;
+        await repository.StoreBasket(cart, cancellationToken);
+
+        return new StoreBasketResult(cart.UserName);
+    }
+}
